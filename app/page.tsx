18ccfa309fa52160c07eec5c3363b2b6d36495a5
Howard Chen <howardchen@MacBookPro.attlocal.net>
2024-12-10 22:59:35 -0800

    Set min height on Module wrap

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Module from "../components/Module";
 export default async function Home() {
   return (
     <Section heightClass="h-max" className="py-20">
-      <Box padding="0" className="h-screen relative items-center">
+      <Box padding="0" className="min-h-screen relative items-center">
         <Module />
       </Box>
     </Section>
