f22f9193aa2e9655010e1a32560561f7d92de250
Howard Chen <howardchen@MacBookPro.attlocal.net>
2024-12-10 22:48:02 -0800

    Add responsive container to bar chart

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BarChart as BarChartCore, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ResponsiveContainer, BarChart as BarChartCore, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 const BarChart = ({ data }) => {
   return (
     <div className="overflow-x-auto">
-      <BarChartCore width={600} height={400} data={data}>
-        <CartesianGrid strokeDasharray="3 3" stroke="#F3F4F6" />
-        <XAxis dataKey="name" stroke="#F3F4F6"/>
-        <YAxis stroke="#F3F4F6"/>
-        <Tooltip cursor={false}/>
-        <Bar dataKey="age" fill="#41ABFF" />
-      </BarChartCore>
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChartCore data={data}>
+          <CartesianGrid strokeDasharray="3 3" stroke="#F3F4F6" />
+          <XAxis dataKey="name" stroke="#F3F4F6"/>
+          <YAxis stroke="#F3F4F6"/>
+          <Tooltip cursor={false}/>
+          <Bar dataKey="age" fill="#41ABFF" />
+        </BarChartCore>
+      </ResponsiveContainer>
     </div>
   );
 }
